b43b39d460055d0f56c38d1915f4347666490c0c
Isloka <71348100+Isloka@users.noreply.github.com>
2021-11-26 21:35:56 +0000

    :)
    :)

--- subdomains.py
+++ subdomains.py
@@ -1,1 +1,1 @@
-list = ["www", "intranet", "www", "	portal", "admin", "www2", "www3", "vpn", "cloud", "teste", "store", "files", "ecommerce", "app", "secure", "smtp", "server", "ftp"]
+list = ["www", "intranet", "www", "portal", "admin", "www2", "www3", "vpn", "cloud", "teste", "store", "files", "ecommerce", "app", "secure", "smtp", "server", "ftp"]
