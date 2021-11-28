2d34d4ed0a22863121a9c1638ec55d5f505fbdca
Isloka <71348100+Isloka@users.noreply.github.com>
2021-11-28 13:27:29 +0000

    :)
    :)

--- main.py
+++ main.py
@@ -2,18 +2,16 @@ import random
 import requests
 import socket
 import domains
-import subdomains
 import defacers
 import useragents
 while True:
   hm = str(random.randint(1, 31))
   r = str(random.randint(1, 7))
   ua = random.choice(useragents.list)
-  sub = random.choice(subdomains.list)
   domain = random.choice(domains.list)
   try:
-    socket.gethostbyname(sub+'.'+domain)
-    finalDomain = 'https://'+sub+'.'+domain+'/'
+    socket.gethostbyname(domain)
+    finalDomain = 'https://'+domain+'/'
     rDefacer = random.choice(defacers.list)
     r = requests.post('http://www.zone-h.org/notify/single', headers={"User-Agent": ua}, data=[('defacer', rDefacer), ('domain1', finalDomain), ('hackmode', hm), ('reason', r)])
     print(rDefacer+' > '+finalDomain+' ['+ua+']')
